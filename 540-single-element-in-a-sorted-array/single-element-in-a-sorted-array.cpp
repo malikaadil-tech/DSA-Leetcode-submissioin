893ac261ea6f933eb7eb1e966503a15e8b7a10a3
Aadil Malik <aadilmalik713@gmail.com>
2026-01-12 23:30:06 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (91.54%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
+    bool isEven(int n) {
+    return n % 2 == 0;
+    }
     int singleNonDuplicate(vector<int>& nums) {
-        // brute force 
+        // (optimisatin through binary search )
         int n = nums.size();
-        if(n==1)return nums[0];
-        for(int i =0;i<n;i++){
-            if(i == 0){
-                if(nums[i]!= nums[i+1]){
-                    return nums[i];
-            }   }
-            else if(i == n-1){
-                    if(nums[i] != nums[i-1]){
-                        return nums[i];
-                    }
-            }    
-            else{
-                    if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]){
-                        return nums[i];
-                    }   
+        // check sepratrely for edge cases to avoid 
+        if(n ==1)return nums[0];
+        // check for first element 
+        if(nums[0]!=nums[1])return nums[0];
+        //check for last element 
+        if(nums[n-1]!=nums[n-2])return nums[n-1];
+        int s = 1;
+        int e = n-2;
+        int mid= s+(e-s)/2;
+        while(s<=e){
+            if(nums[mid] != nums[mid+1] && nums[mid] !=nums[mid-1]){
+                return nums[mid];
             }
-           
+            else if((mid % 2!=0 && nums[mid] == nums[mid-1]) || (mid%2 == 0 && nums[mid] == nums[mid+1])){
+                // i m on the left half and element on the right half so remove left half
+                s = mid+1;
+            }  
+            else{
+                // so element on the left half 
+                e = mid-1;
+            } 
+            mid = s+(e-s)/2;  
         }
-        
+
+
                 
       return -1;  
        
