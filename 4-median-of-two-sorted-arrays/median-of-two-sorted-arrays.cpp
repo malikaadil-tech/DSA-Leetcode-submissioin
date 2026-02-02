540c8698263f1ef3efc3ad51fe9900fd06963687
Aadil Malik <aadilmalik713@gmail.com>
2026-02-03 01:00:21 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (93.88%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,39 +1,54 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    int i = 0, j = 0;
-    vector<int> ans;
-    while (i < nums1.size() && j < nums2.size()) {
-        if (nums1[i] <=nums2[j]) {
-            ans.push_back(nums1[i]);
-            i++;
-        } else {
-            ans.push_back(nums2[j]);
-            j++;
+        int n  = nums1.size();
+        int m = nums2.size();
+        int k = n+m;
+        int index2 = k/2;
+        int index1= index2-1;
+        int i =0;
+        int j = 0;
+        int indexele1 = -1;
+        int indexele2 = -1;
+        int cnt =0;
+        while(i<n && j<m){
+            if(nums1[i]< nums2[j]){
+                if(cnt == index1) indexele1 = nums1[i];
+                if(cnt  == index2) indexele2 = nums1[i];
+                cnt++;
+                i++;
+             }
+            else{ if(cnt == index1) indexele1 = nums2[j];
+                if(cnt  == index2) indexele2 = nums2[j];
+                cnt++;
+                j++;
+            }
         }
-    }
+        //remaining elements 
+        while(i<n){
+            if(cnt == index1) indexele1 = nums1[i];
+            if(cnt  == index2) indexele2 = nums1[i];
+                cnt++;
+                i++;
+        }
+        while(j<m){
+            if(cnt == index1) indexele1 = nums2[j];
+            if(cnt  == index2) indexele2 = nums2[j];
+                cnt++;
+                j++;
+        }
+        // check whether k is odd or even npw 
+        if(k%2 == 1){
+            // k is odd
+            return indexele2;
+        }
+        else{
+            return (double)(double)(indexele1 + indexele2)/2.0;
+        }
 
-    // remaining elements
-    while (i <  nums1.size()) {
-        ans.push_back(nums1[i]);
-        i++;
-    }
 
-    while (j <  nums2.size()) {
-        ans.push_back(nums2[j]);
-        j++;
-    }
-    int n = ans.size();
-    // median calculation 
-    if(n%2 == 1){
-        //odd number of elements 
-        return ans[n/2];
-    }
-    else{
-        // even number of elements 
-        
-        return (ans[n/2-1]+ans[n/2])/2.0;
-    }
+
+    
         
     }
 };
